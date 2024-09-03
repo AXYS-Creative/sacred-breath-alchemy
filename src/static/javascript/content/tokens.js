d3d00dce06e42dec8b9bbeb7667eb1d074bcfd40
Aaron Garcia <axyscreativeagency@gmail.com>
2024-09-03 08:03:01 -0600

    Updated linebreak token to work with voice over

--- src/static/javascript/content/tokens.js
+++ src/static/javascript/content/tokens.js
@@ -1,38 +1,25 @@
-// CMS Token - Any string element can use the token '[%br%]' to wrap all previous text within a span.
-const lineWrapToken = (() => {
-  const lineWrap = (inputString) => {
-    // Using the token [%br%] on the CMS side
-    const segments = inputString.split("[%br%]");
-
-    let processedSegments = [];
-
-    segments.forEach((segment) => {
-      if (segment.trim() !== "") {
-        processedSegments.push(
-          `<span class="token-br">${segment.trim()}</span>`
-        );
-      }
-    });
-
-    return processedSegments.join("");
+// CMS Token - Any string element can use the token '[%br%]' to inject a <br> tag.
+const lineBreakToken = (() => {
+  const lineBreak = (inputString) => {
+    return inputString.split("[%br%]").join(`<br aria-hidden="true">`);
   };
 
   const elements = document.querySelectorAll(`
-    p,
-    span,
-    h1, 
-    h2, 
-    h3, 
-    h4, 
-    h5, 
-    h6
-  `);
+        p,
+        span,
+        h1, 
+        h2, 
+        h3, 
+        h4, 
+        h5, 
+        h6
+    `);
 
   elements.forEach((element) => {
     const content = element.innerHTML;
 
     if (content.includes("[%br%]")) {
-      const wrappedContent = lineWrap(content);
+      const wrappedContent = lineBreak(content);
       element.innerHTML = wrappedContent;
     }
   });
