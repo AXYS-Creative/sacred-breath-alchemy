700b7432db42a2bcba64c254972024ec676b0c67
Aaron Garcia <axyscreativeagency@gmail.com>
2024-09-14 12:04:48 -0600

    removed video passthrough and asset

--- .eleventy.js
+++ .eleventy.js
@@ -32,7 +32,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/static/styles");
   eleventyConfig.addPassthroughCopy("./src/static/fonts");
   eleventyConfig.addPassthroughCopy("./src/static/img");
-  eleventyConfig.addPassthroughCopy("./src/static/vid");
   eleventyConfig.addPassthroughCopy("./src/static/javascript");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
 
